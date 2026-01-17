71ebe27983c7dd5af423aed7021a2c973aabf424
Ankita Gupta <ankita@Ankitas-MacBook-Air.local>
2026-01-17 14:58:18 +0530

    Initial setup for server

---
+++ prompt-architect-server/src/main/java/com/example/prompt_architect_server/PromptArchitectServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.prompt_architect_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptArchitectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptArchitectServerApplication.class, args);
+	}
+
+}
---
+++ prompt-architect-server/src/test/java/com/example/prompt_architect_server/PromptArchitectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prompt_architect_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptArchitectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
